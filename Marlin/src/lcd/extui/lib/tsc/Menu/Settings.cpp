b892eeb97adecbd37fb2f204915c0df18050f62d
Paulo <peekpt@gmail.com>
2026-09-21 13:14:24 +0100

    fix(tsc): Info screen firmware row shows 'MMM 2.1 - <build date>'

--- Marlin/src/lcd/extui/lib/tsc/Menu/Settings.cpp
+++ Marlin/src/lcd/extui/lib/tsc/Menu/Settings.cpp
@@ -70,7 +70,7 @@ void menuCallBackInfo(void)
 void menuInfo(void)
 {
   const char* hardware = "Board   : " BOARD_INFO_NAME ROBOT_NAME CHIP_NAME;
-  const char* firmware = "Firmware: " SHORT_BUILD_VERSION MATERIALS_NAME " (" __DATE__") ";
+  const char* firmware = "MMM 2.1 - " __DATE__;
   const char* marlin = "Marlin  : " SHORT_BUILD_VERSION;
   const char* orangeMod = "ORANGE MOD";
   
