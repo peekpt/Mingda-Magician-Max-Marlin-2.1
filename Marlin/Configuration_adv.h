03dbd18b3acfe56065a0066a7fc2efc71c50a633
Paulo <peekpt@gmail.com>
2026-09-22 19:38:38 +0100

    config(adv): enable LIN_ADVANCE, ADVANCE_K=0.035, ALLOW_LOW_EJERK
    DEFAULT_EJERK is 7.0 (<10), so ALLOW_LOW_EJERK is required.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1743,12 +1743,17 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-//#define LIN_ADVANCE
+#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
-  //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define ADVANCE_K 0    // Unit: mm compression per 1mm/s extruder speed
-  //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
-  // #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
+  #if ENABLED(DISTINCT_E_FACTORS)
+    #define ADVANCE_K { 0.035 }   // (mm) Compression length per 1mm/s extruder speed, per extruder
+  #else
+    #define ADVANCE_K 0.035       // (mm) Compression length applying to all extruders
+  #endif
+  //#define ADVANCE_K_EXTRA       // Add a second linear advance constant, configurable with M900 L.
+  //#define LA_DEBUG              // Print debug information to serial during operation. Disable for production use.
+  #define ALLOW_LOW_EJERK         // Allow a DEFAULT_EJERK value of <10. Recommended for direct drive hotends.
+  //#define EXPERIMENTAL_I2S_LA   // Allow I2S_STEPPER_STREAM to be used with LA.
 #endif
 
 // @section leveling
