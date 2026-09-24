4347aedfb3da62acaecb5a734a3c00dcd44ec37e
Paulo <peekpt@gmail.com>
2026-09-24 12:17:25 +0100

    diag: TMC_DEBUG + MONITOR_DRIVER_STATUS, revert XY to StealthChop
    X still stalls. Enable M122/MONITOR_DRIVER_STATUS to read the X driver state,
    and restore StealthChop on XY (matches 2.0.x; removes spreadCycle buzz).

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2484,7 +2484,7 @@
    * Use Trinamic's ultra quiet stepping mode.
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
-  //#define STEALTHCHOP_XY   // XY in spreadCycle (more torque / robust against sync loss)
+  #define STEALTHCHOP_XY
   #define STEALTHCHOP_Z
   #define STEALTHCHOP_E
 
@@ -2516,12 +2516,12 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 - Report driver parameters (Requires TMC_DEBUG)
    */
-  //#define MONITOR_DRIVER_STATUS
+  #define MONITOR_DRIVER_STATUS
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
     #define REPORT_CURRENT_CHANGE
-    #define STOP_ON_ERROR
+    //#define STOP_ON_ERROR          // (diagnostic) keep printing and report
   #endif
 
   /**
@@ -2613,7 +2613,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continous reporting.
    */
-  //#define TMC_DEBUG
+  #define TMC_DEBUG
 
   /**
    * You can set your own advanced settings by filling in predefined functions.
